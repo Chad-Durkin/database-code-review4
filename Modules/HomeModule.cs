e64fae1b0a33d388c0149592e59c3f20bbbdbbc1
Chad Durkin <Chaddurkin@gmail.com>
2017-03-03 10:02:08 -0800

    basic design/format set up, home and add band methods set up in the home module

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
 
-namespace LibraryCatalog
+namespace Bandtracker
 {
     public class HomeModule : NancyModule
     {
         public HomeModule()
         {
             Get["/"] = _ => {
-                return View["index.cshtml"];
+                Dictionary<string, object> model = new Dictionary<string, object>{};
+                model.Add("bands", Band.GetAll());
+                model.Add("venues", Venue.GetAll());
+                return View["index.cshtml", model];
+            };
+            Get["/band/add"] = _ => {
+                return View["band_form.cshtml"];
+            };
+            Post["/band/add"] = _ => {
+                Band newBand = new Band(Request.Form["band-name"]);
+                newBand.Save();
+                Dictionary<string, object> model = new Dictionary<string, object>{};
+                model.Add("bands", Band.GetAll());
+                model.Add("venues", Venue.GetAll());
+                return View["index.cshtml", model];
             };
         }
     }
