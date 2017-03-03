3a85d52d70e65cf40c012caa16d3ff19702c2de6
Chad Durkin <Chaddurkin@gmail.com>
2017-03-03 10:16:10 -0800

    add update/delete methods to the homemodule for bands

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -26,6 +26,37 @@ namespace Bandtracker
                 model.Add("venues", Venue.GetAll());
                 return View["index.cshtml", model];
             };
+            Get["/venue/add"] = _ => {
+                return View["venue_form.cshtml"];
+            };
+            Post["/venue/add"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                Dictionary<string, object> model = new Dictionary<string, object>{};
+                model.Add("bands", Band.GetAll());
+                model.Add("venues", Venue.GetAll());
+                return View["index.cshtml", model];
+            };
+            Get["/band/{id}"] = parameters => {
+                Band model = Band.FindBand(parameters.id);
+                return View["band.cshtml", model];
+            };
+            Patch["/band/update/{id}"] = parameters => {
+                Band updateBand = Band.FindBand(parameters.id);
+                updateBand.UpdateBand(Request.Form["band-name"]);
+                Dictionary<string, object> model = new Dictionary<string, object>{};
+                model.Add("bands", Band.GetAll());
+                model.Add("venues", Venue.GetAll());
+                return View["index.cshtml", model];
+            };
+            Delete["/band/delete/{id}"] = parameters => {
+                Band deleteBand = Band.FindBand(parameters.id);
+                deleteBand.DeleteBand();
+                Dictionary<string, object> model = new Dictionary<string, object>{};
+                model.Add("bands", Band.GetAll());
+                model.Add("venues", Venue.GetAll());
+                return View["index.cshtml", model];
+            };
         }
     }
 }
